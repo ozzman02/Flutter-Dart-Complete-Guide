1b8f1e907ef34030cba77edbd8589371bbdffa1b
Oscar Santamaria Venegas <osantamaria@gmail.com>
2024-11-24 18:24:00 -0600

    Flutter & Dart Basics Ib

--- first_app/lib/gradient_container.dart
+++ first_app/lib/gradient_container.dart
@@ -1,24 +1,53 @@
-import 'package:first_app/styled_text.dart';
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
-class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+/*class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98)
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          colors: colors,
+          begin: startAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(
-        child: StyleText(),
+        child: StyleText("Hello World !!!"),
+      ),
+    );
+  }
+}*/
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  const GradientContainer.purple({super.key})
+      : color1 = Colors.deepPurple,
+        color2 = Colors.indigo;
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [color1, color2],
+          begin: startAlignment,
+          end: endAlignment,
+        ),
+      ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
